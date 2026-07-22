c64ffcc543ca7ce489196c1437f8e702b205759e
Rick-and-Morty-Jase <joshuakellerman0@gmail.com>
2026-07-22 20:20:04 +0200

    Removed the code

--- engine/size.js
+++ engine/size.js
@@ -1,6 +1,1 @@
-let navH=document.getElementById("nav").offsetHeight;
-let para=document.getElementById("para").offsetHeight;
-let h1H=document.getElementById("heading").offsetHeight;
-let setH=window.innerHeight-(navH+para+h1H);
-let section1=document.getElementById("section1");
-section1.setAttribute("style","height:"+setH+"px;")
+//dummy file now
