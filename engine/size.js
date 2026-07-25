9eab029651fb477aa3f85f6555d49b2360c4752a
rick-&-morty-jase <joshuakellermam0@gmail.com>
2026-07-25 11:25:48 +0200

    Updated

--- engine/size.js
+++ engine/size.js
@@ -1,1 +1,27 @@
 //dummy file now
+let windowW1=window.innerWidth;
+let phone=false;
+let navBar=document.getElementById("nav");
+if(windowW1<500)
+{
+  phone=true;
+  let i1=document.getElementById("i1");
+  let i2=document.getElementById("i2");
+  let i3=document.getElementById("i3");
+  let i4=document.getElementById("i4");
+  
+  i1.remove();
+  i2.remove();
+  i3.remove();
+  i4.remove();
+  let sec1=document.getElementById("section1");
+  section1.setAttribute("style","position:static;");
+  let sec=document.getElementById("sections");
+  sec.setAttribute("style","display:flex;flex-direction:column;color:red;");
+  
+
+}
+else
+{
+  
+}
